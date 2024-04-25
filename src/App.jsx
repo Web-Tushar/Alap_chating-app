faa247d30d84426f58011fd7d391a1aaad74a631
Tushar <153347682+Web-Tushar@users.noreply.github.com>
2024-04-26 00:18:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Button from '@mui/material/Button';
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,RouterProvider,
+} from "react-router-dom";
+import Login from './pages/auth/Login';
+import Error from './pages/Error';
+import Rootlayout from './components/layouts/Rootlayout';
+ import Registration from './pages/auth/Registration';
+import Home from './pages/home/Home';
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+        <Route element={<Rootlayout/>}>
+            < Route  path='/Home' element={<Home/>}/>                                        
+        </Route>
+            {/* <Route path='/Registration' element={<Registration/>}   />  */}
+            <Route path='*' element={<Error/>}/> 
+            <Route path="/" element={<Login  />}/>
+      </Route>
+    
+    
+  )
+);
+function App() {
+
+
+  return (
+    <>  <RouterProvider
+            router={router}
+  />
+    </>
+  )
+}
+
+export default App
